c08b66af7d3a9bb47c2ccc622fd7a4ba24cac6c5
William Wong <rightedges@gmail.com>
2025-11-25 11:59:03 -0800

    Ensure the db is created for production

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ app = create_app()
 def make_shell_context():
     return {'db': db, 'User': User, 'Portfolio': Portfolio, 'Holding': Holding}
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5001)
